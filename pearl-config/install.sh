372eaeeeb521c1c0511a3ce5c399cb7669989a6f
Filippo Squillace <squillace.filippo@gmail.com>
2019-05-12 22:26:08 +1000

    first commit

---
+++ pearl-config/install.sh
@@ -0,0 +1,34 @@
+function post_install(){
+    local pkgname=trash-cli
+    local giturl=https://github.com/andreafrancia/trash-cli.git
+    local location="${PEARL_PKGVARDIR}/trash-cli"
+
+    info "Installing or updating the $pkgname git repository..."
+    install_or_update_git_repo $giturl "$location" master
+
+    cd "$location"
+    python setup.py --quiet build
+
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash-list
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash-empty
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash-put
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash-restore
+    link_to_path ${PEARL_PKGVARDIR}/trash-cli/trash-rm
+
+    return 0
+}
+
+function post_update(){
+    post_install
+}
+
+function pre_remove(){
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash-list
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash-empty
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash-put
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash-restore
+    unlink_from_path ${PEARL_PKGVARDIR}/trash-cli/trash-rm
+    rm -rf "${PEARL_PKGVARDIR}/trash-cli"
+}
